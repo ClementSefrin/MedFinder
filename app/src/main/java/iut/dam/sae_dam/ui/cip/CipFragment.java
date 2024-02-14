ef0b38da889b337caa808d5d54052b711408e752
ClementSefrin <clementsefrin.pro@gmail.com>
2024-02-14 11:33:09 +0100

    Ajout de la page de signalement avec le code CIP

--- app/src/main/java/iut/dam/sae_dam/ui/cip/CipFragment.java
+++ app/src/main/java/iut/dam/sae_dam/ui/cip/CipFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,8 +21,6 @@ public class CipFragment extends Fragment {
         binding = FragmentCipBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textCip;
-        cipViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
