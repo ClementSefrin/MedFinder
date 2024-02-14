50fa8fe2c8ad140bf3cc89d51dae8ccdfef3ede5
ClementSefrin <clementsefrin.pro@gmail.com>
2024-02-14 13:31:46 +0100

    Changements mineurs

--- app/src/main/java/iut/dam/sae_dam/MainActivity.java
+++ app/src/main/java/iut/dam/sae_dam/MainActivity.java
@@ -1,11 +1,15 @@
 package iut.dam.sae_dam;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -24,14 +28,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_scan, R.id.navigation_cip, R.id.navigation_account)
-                .build();
+                .build();*/
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        // Update header text dynamically
+        final TextView headerTextView = findViewById(R.id.header_title);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller,
+                                             @NonNull NavDestination destination, Bundle arguments) {
+                // Set the header text based on the destination's label
+                headerTextView.setText(destination.getLabel());
+            }
+        });
+    }
 }
